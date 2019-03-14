890faa4dc7a8b61ad5d4094cb688966dd6a1cb67
Jose Macias <josemariamacias@gmail.com>
2019-03-14 10:30:35 +0000

    Refactoring menu in print_menu and process methods

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-students = [
+@students = [
   {name: "Akuba", cohort: :march},
   {name: "Philip Board", cohort: :march},
   {name: "Josh", cohort: :march},
@@ -13,56 +13,58 @@ def print_header
   puts "-------"
 end
 
-def print_names(students)
-  students.each_with_index { |student, index| puts "#{index +1}: #{student[:name]} (#{student[:cohort]} cohort)" }
+def print_students_list
+  @students.each_with_index { |student, index| puts "#{index +1}: #{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students."
+def print_footer
+  puts "Overall, we have #{@students.count} great students."
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  students = []
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :march}
-    puts "Now we have #{students.count} students."
+    @students << {name: name, cohort: :march}
+    puts "Now we have #{@students.count} students."
     name = gets.chomp
   end
+end
 
-  students
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "That option doesn't exist. Try again."
+  end
 end
 
 def interactive_menu
-  students = []
-
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
-    selection = gets.chomp
-
-    case selection
-    when "1"
-      students += input_students
-    when "2"
-      print_header
-      print_names(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "That option doesn't exist. Try again."
-    end
-
+    print_menu
+    process(gets.chomp)
   end
-
 end
+
 
 interactive_menu
