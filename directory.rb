231b3f84f4d6d6051dc918538c9928a6f59df792
Jose Macias <josemariamacias@gmail.com>
2019-03-14 09:22:55 +0000

    List of students and put it

--- directory.rb
+++ directory.rb
@@ -1,3 +1,14 @@
-student_count = 11
+student_count = 7
 
+students = [
+  "Akuba",
+  "Philip Board",
+  "Josh",
+  "Sol",
+  "Sean",
+  "Deanna",
+  "Jose"
+]
+
+students.each { |name| puts name }
 puts "Overall, we have #{student_count} great students."
